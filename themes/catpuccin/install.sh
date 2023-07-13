7c834547988635147757bba215c1c32d41d920a5
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-07-13 21:24:22 +0300

    New setup step by step guide

---
+++ themes/catpuccin/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p $HOME/.config/zathura
+cp picom.conf $HOME/.config/zathura/zathurarc
+
+cp Xresources $HOME/.Xresources
+
+mkdir -p $HOME/.local/share/wallpapers
+cp wallpapers/*  $HOME/.local/share/wallpapers/
+
