bf14cb4063c188138bf18de0b55c6c9566ae7aa2
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-07-15 04:48:44 +0300

    bunch of changes

--- themes/catpuccin/mocha/install.sh
+++ themes/catpuccin/mocha/install.sh
@@ -9,6 +9,9 @@ cp dunstrc $HOME/.config/dunst/dunstrc
 mkdir -p $HOME/.config/picom
 cp picom.conf $HOME/.config/picom/picom.conf
 
+mkdir -p $HOME/.config/qt5ct/colors
+cp qt5ct $HOME/.config/qt5ct/colors/Catpuccin-Mocha.conf
+
 cp Xresources $HOME/.Xresources
 
 mkdir -p $HOME/.local/share/wallpapers
