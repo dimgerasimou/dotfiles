b143cee94405a8b68b27fa1c5a1417756b67e077
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-08-01 23:56:37 +0300

    Changed Xresources structure

--- themes/catpuccin/mocha/install.sh
+++ themes/catpuccin/mocha/install.sh
@@ -1,19 +1,62 @@
 #!/bin/sh
 
-mkdir -p $HOME/.config/zathura
-cp zathurarc $HOME/.config/zathura/zathurarc
+if [ $1 == "uninstall" ]; then
+	uninstall()
+else if [ $1 == "update" ]; then
+	update()
+else
+	install()
+fi
 
-mkdir -p $HOME/.config/dunst
-cp dunstrc $HOME/.config/dunst/dunstrc
+install() {
+	mkdir -p $HOME/.config/zathura
+	cp zathurarc $HOME/.config/zathura/zathurarc
+	
+	mkdir -p $HOME/.config/dunst
+	cp dunstrc $HOME/.config/dunst/dunstrc
+	
+	mkdir -p $HOME/.config/picom
+	cp picom.conf $HOME/.config/picom/picom.conf
+	
+	mkdir -p $HOME/.config/qt5ct/colors
+	cp qt5ct $HOME/.config/qt5ct/colors/Catpuccin-Mocha.conf
+	
+	cp Xresources $HOME/.Xresources
+	cp -r Xresources.d $HOME/.Xresources.d/
+	
+	mkdir -p $HOME/.local/share/wallpapers
+	cp wallpapers/*  $HOME/.local/share/wallpapers/
+}
 
-mkdir -p $HOME/.config/picom
-cp picom.conf $HOME/.config/picom/picom.conf
+update() {
+	cp $HOME/.config/zathura/zathurarc zathurarc
 
-mkdir -p $HOME/.config/qt5ct/colors
-cp qt5ct $HOME/.config/qt5ct/colors/Catpuccin-Mocha.conf
+	cp $HOME/.config/dunst/dunstrc dunstrc
 
-cp Xresources $HOME/.Xresources
+	cp $HOME/.config/picom/picom.conf picom.conf
 
-mkdir -p $HOME/.local/share/wallpapers
-cp wallpapers/*  $HOME/.local/share/wallpapers/
+	cp $HOME/.config/qt5ct/colors/Catpuccin-Mocha.conf qt5ct
 
+	cp $HOME/.Xresources Xresources
+
+	rm -r Xresources.d
+	cp -r $HOME/.Xresources.d/ Xresources.d
+}
+
+uninstall() {
+	rm $HOME/.config/zathura/zathurarc
+
+	rm $HOME/.config/dunst/dunstrc
+
+	rm $HOME/.config/picom/picom.conf
+
+	rm HOME/.config/qt5ct/colors/Catpuccin-Mocha.conf
+
+	rm $HOME/.Xresources
+	rm -r $HOME/.Xresources.d
+
+	list="$(ls wallpapers)"
+	for $image in $list; do
+		rm $HOME/.local/share/wallpapers
+	done
+}
